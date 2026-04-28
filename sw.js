ab765b2004b2a5e073ce1980430154480ef665a3
zvxltt <valentintrywork@gmail.com>
2026-04-28 23:55:41 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,10 @@
+const cacheName = 'jeu-v1';
+const assets = ['./', './index.html', './script.js'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
