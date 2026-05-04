307f98d0ba9d2c794c408912d85fa74bb6db0bfa
zvxltt <valentintrywork@gmail.com>
2026-05-04 20:38:48 +0200

    Update service worker cache name and assets list

--- sw.js
+++ sw.js
@@ -1,10 +1,24 @@
-const cacheName = 'jeu-v1';
-const assets = ['./', './index.html', './script.js'];
+const cacheName = 'Zero-To-Empire';
+// Liste tous les fichiers de ton jeu ici (index.html, images, sons, etc.)
+const assets = [
+  './',
+  './index.html', 
+];
 
+// Installation : Mise en cache des fichiers
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
 });
 
+// Stratégie : Répondre avec le cache si hors-ligne, sinon utiliser le réseau
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
 });
